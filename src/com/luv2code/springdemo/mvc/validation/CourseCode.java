ce5f9d1b222365448cac513b33a40ba1803feb06
youngminshin <youngminshin@192.168.10.9>
2019-08-12 07:01:59 +0900

    Custom validation - from udemy

--- src/com/luv2code/springdemo/mvc/validation/CourseCode.java
+++ src/com/luv2code/springdemo/mvc/validation/CourseCode.java
@@ -9,19 +9,19 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 
 @Constraint(validatedBy = CourseCodeConstraintValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
+
 	// define default course code
 	public String value() default "LUV";
-	
+
 	// define default error message
-	public String message() default "must starts with LUV";
-	
-	// define default group
-	public Class<?>[] gourps() default {};
-	
+	public String message() default "must start with LUV";
+
+	// define default groups
+	public Class<?>[] groups() default {};
+
 	// define default payloads
 	public Class<? extends Payload>[] payload() default {};
-
 }
--- src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -7,12 +7,14 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
 
 	private String coursePrefix;
 
+	@Override
 	public void initialize(CourseCode theCourseCode) {
 		coursePrefix = theCourseCode.value();
 	}
 
 	@Override
 	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+
 		boolean result;
 
 		if (theCode != null) {
@@ -20,7 +22,7 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
 		} else {
 			result = true;
 		}
+
 		return result;
 	}
-
 }
